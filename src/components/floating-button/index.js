62449502050911c5e29c176313e5edbe1202c10e
Donggeun Lee <eastroots92@gmail.com>
2020-08-14 22:11:35 +0900

    Revert "Change floating button link"
    This reverts commit 80c2bcd30de2618fa2e77aaed16ddbbd373547d1.

--- src/components/floating-button/index.js
+++ src/components/floating-button/index.js
@@ -1,17 +1,17 @@
 import S from './styles';
 import React from "react"
-import youtube from '../../resources/youtube_b.svg';
+import scheduleIcon from '../../resources/schedule.svg';
 import { GA_ACTION, GAClickEvent } from "../../utils/ga"
 
-const youtubeLink = 'https://youtu.be/i0FMti8pOsk';
+const downlaodLink = 'https://letusgo.s3.ap-northeast-2.amazonaws.com/letUsGo2020.ics';
 
 export const FloatingButton = () => (
   <S.Button
-    href={youtubeLink}
+    href={downlaodLink}
     target='_blank'
-    onClick={ GAClickEvent('플로팅_버튼', GA_ACTION.CLICK, '유튜브') }
+    onClick={ GAClickEvent('플로팅_버튼', GA_ACTION.CLICK, '스케쥴') }
   >
-    <S.Icon src={youtube} />
-    <span>Live Seminar</span>
+    <S.Icon src={scheduleIcon} />
+    <span>Add Schedule</span>
   </S.Button>
 )
