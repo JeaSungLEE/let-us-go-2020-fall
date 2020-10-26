e7e03a4511a0927a77ac8f65177f4cf8de710d02
Jercy <wotjdzz1@naver.com>
2020-10-26 11:42:03 +0900

    스케쥴 링크 변경

--- src/components/floating-button/index.js
+++ src/components/floating-button/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import scheduleIcon from '../../resources/schedule.svg';
 import { GA_ACTION, GAClickEvent } from "../../utils/ga"
 
-const downlaodLink = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/db40ad0a-a849-4085-9526-f8c9a9c66f1a/iCal-20201022-211304.ics?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201022%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201022T121544Z&X-Amz-Expires=86400&X-Amz-Signature=d2b262c5e5fde8f8c00bfd1fc6e65d5d3579d5c7dfc25e7d6be7fbfaa48fe7cf&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22iCal-20201022-211304.ics%22';
+const downlaodLink = 'https://drive.google.com/uc?id=1tq1m3myzWKDEaMJgopxjVEhPd9FrhzKJ&authuser=0&export=download';
 
 export const FloatingButton = () => (
   <S.Button
